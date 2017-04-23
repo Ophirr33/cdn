104b49781caad8872b3c16010dbbcae7105ae63e
Ty Coghlan <Coghlan.ty@gmail.com>
2017-04-22 23:42:34 -0400

    http servers now populate and use their caches

---
+++ src/cdn/httpserver/cache.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// errorCheck is a convenience method that will print to standard error if err is an Error
+func errorCheck(err error) bool {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err)
+		return true
+	}
+	return false
+}
+
+type cache struct {
+	memCacheSize         uint
+	diskCacheSize        uint
+	currentMemCacheSize  uint
+	currentDiskCacheSize uint
+	memCache             map[string]*http.Response
+	diskCache            map[string]string // paths to file names
+	built                bool
+}
+
+func (cache *cache) init(memCacheSize, diskCacheSize uint) {
+	cache.memCacheSize = memCacheSize
+	cache.diskCacheSize = diskCacheSize
+	cache.currentMemCacheSize = 0
+	cache.currentDiskCacheSize = 0
+	cache.memCache = make(map[string]*http.Response)
+	cache.diskCache = make(map[string]string)
+	cache.built = false
+}
+
+func (cache *cache) addToCache(path string, resp *http.Response) bool {
+	path = strings.ToLower(path)
+	if cache.containsPath(path) {
+		return false
+	}
+	return cache.containsPath(path) &&
+		(cache.addToMemCache(path, resp) ||
+			cache.addToDiskCache(path, resp))
+}
+
+func (cache *cache) addToMemCache(path string, resp *http.Response) bool {
+	if _, inMem := cache.memCache[path]; resp.ContentLength == -1 || inMem ||
+		resp.ContentLength > int64(cache.memCacheSize-cache.currentMemCacheSize) {
+		return false
+	}
+	cache.memCache[path] = resp
+	cache.currentMemCacheSize += uint(resp.ContentLength)
+	return true
+}
+
+func (cache *cache) addToDiskCache(path string, resp *http.Response) bool {
+	if _, inDisk := cache.diskCache[path]; resp.ContentLength == -1 || inDisk ||
+		resp.ContentLength > int64(cache.diskCacheSize-cache.currentDiskCacheSize) {
+		return false
+	}
+	fileName := strings.Replace(path, "/", "_", -1) + ".txt"
+	f, err := os.Create(fileName)
+	if errorCheck(err) {
+		return false
+	}
+	defer f.Close()
+	err = resp.Write(f)
+	if errorCheck(err) {
+		return false
+	}
+	cache.diskCache[path] = fileName
+	cache.currentDiskCacheSize += uint(resp.ContentLength)
+	return true
+}
+
+// containsPath returns whether the path is in the cache, and if it is,
+// it returns whether it is in the memory or disk cache
+func (cache *cache) containsPath(path string) bool {
+	path = strings.ToLower(path)
+	if !cache.built {
+		return false
+	}
+	_, inMem := cache.memCache[path]
+	_, inDisk := cache.diskCache[path]
+	return inMem || inDisk
+}
+
+// getFromCache returns the cached http response
+func (cache *cache) getFromCache(path string) (*http.Response, error) {
+	if !cache.built {
+		return nil, errors.New("Cache has not been built yet")
+	}
+	path = strings.ToLower(path)
+	if !cache.containsPath(path) {
+		return nil, errors.New("Cache does not contain path `" + path + "`")
+	} else if _, inMem := cache.memCache[path]; inMem {
+		return cache.memCache[path], nil
+	}
+	fileName := cache.diskCache[path]
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	resp, err := http.ReadResponse(bufio.NewReader(f), nil)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (cache *cache) buildCache(origin, popularFileName string) {
+	client := &http.Client{}
+	f, err := os.Open(popularFileName)
+	if errorCheck(err) {
+		return
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		path := strings.Fields(scanner.Text())[0]
+		resp, err := client.Get(origin + path)
+		if !errorCheck(err) {
+			cache.addToCache(path, resp)
+		}
+	}
+	cache.built = true // Allow the cache to be used
+	fmt.Println("Built cache")
+}
--- src/cdn/httpserver/httpserver.go
+++ src/cdn/httpserver/httpserver.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
@@ -23,7 +24,7 @@ func errorCheck(err error) bool {
 
 // httpServer takes in the port and the url of the origin server
 // It initializes a tcp socket and spawns go routines to handle incoming connections
-func httpServer(port int, origin string) {
+func httpServer(port int, origin string, cache *cache) {
 	var signals = make(chan os.Signal, 1)
 	var conns = make(chan *net.TCPConn, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
@@ -51,7 +52,7 @@ func httpServer(port int, origin string) {
 			if !ok {
 				return
 			}
-			go handleConnection(connection, origin, client)
+			go handleConnection(connection, origin, client, cache)
 		case <-signals:
 			listener.Close()
 			return
@@ -61,13 +62,25 @@ func httpServer(port int, origin string) {
 
 // handleConnection sends the incoming http request to the origin server
 // In the future it will filter incoming connections through a caching layer
-func handleConnection(connection *net.TCPConn, origin string, client *http.Client) {
+func handleConnection(connection *net.TCPConn, origin string, client *http.Client, cache *cache) {
 	defer connection.Close()
 	req, err := http.ReadRequest(bufio.NewReader(connection))
 	if errorCheck(err) {
 		return
 	}
-	resp, err := client.Get(origin + req.URL.RequestURI())
+	err = nil
+	var resp *http.Response
+	path := strings.ToLower(req.RequestURI)
+	if cache.containsPath(path) {
+		resp, err = cache.getFromCache(path)
+		if !errorCheck(err) {
+			resp.Write(connection)
+			errorCheck(err)
+			return
+		}
+		// If there's an error then we try to grab it from the origin
+	}
+	resp, err = client.Get(origin + path)
 	if errorCheck(err) {
 		return
 	}
@@ -95,7 +108,11 @@ func main() {
 			return
 		}
 	}
+	var bytesInMegabyte uint = 1000000
+	cache := &cache{}
+	cache.init(10*bytesInMegabyte, 6*bytesInMegabyte)
+	go cache.buildCache(*origin, "popular.txt")
 	fmt.Println(*port, *origin)
-	httpServer(*port, *origin)
+	httpServer(*port, *origin, cache)
 	fmt.Println("Exiting...")
 }
