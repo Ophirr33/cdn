7dae7165d6fea643c4566eb4082ee117295dcac7
cdris <cdris@ccs.neu.edu>
2017-04-23 20:36:16 -0400

    sqlite3 errors go to null

--- download_and_create_db.sh
+++ download_and_create_db.sh
@@ -1,6 +1,6 @@
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity_CSV/GeoLiteCity-latest.tar.xz &&
   tar xf GeoLiteCity-latest.tar.xz &&
   mv GeoLiteCity_20170404/*.csv . &&
-  sqlite3 locations.db < create_locations_db.sql &&
+  sqlite3 locations.db < create_locations_db.sql 2> /dev/null &&
   rm *.csv &&
   rm -r GeoLite*
